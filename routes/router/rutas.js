96f1bda2ac8e53943f0a7b6b160ade8e990a15af
luis <lruise.1@gmail.com>
2019-10-24 15:00:13 -0400

    objeto copn rutas creado mas values

--- routes/router/rutas.js
+++ routes/router/rutas.js
@@ -1,329 +1,328 @@
 module.exports=[{
   key:'Camiri-Monteagudo',
   value:126
-},
-{
+},{
   key:'Camiri-Villamontes',
   value:162
 },{
   key:'Cbba-Bermejo',
-  value:126
+  value:1030
 },{
   key:'Cbba-Camiri',
-  value:126
+  value:767
 },{
   key:'Cbba-La Paz',
-  value:126
+  value:392
 },{
   key:'Cbba-Orudo',
-  value:126
+  value:208
 },{
   key:'Cbba-Potosi',
-  value:126
+  value:512
 },{
   key:'Cbba-Pto. Villarroel',
-  value:126
+  value:259
 },{
   key:'Cbba-Pto. Villarroel (Por SZC carr ant)',
-  value:126
+  value:752
 },{
   key:'Cbba-San Jose de Chiquitos',
-  value:126
+  value:738
 },{
   key:'Cbba-Santa Cruz',
-  value:126
+  value:475
 },{
   key:'Cbba-Santa Cruz (carr. Antigua)',
-  value:126
+  value:482
 },{
   key:'Cbba-Sucre (Por Mizque - Aiquile)',
-  value:126
+  value:338
 },{
   key:'Cbba-Sucre (por Potosi)',
-  value:126
+  value:659
 },{
   key:'Cbba-Tarija',
-  value:126
+  value:847
 },{
   key:'Cbba-Trinidad',
-  value:126
+  value:1019
 },{
   key:'Cbba-Trinidad (carr. antigua)',
-  value:126
+  value:1026
 },{
   key:'Cbba-Tupiza',
-  value:126
+  value:847
 },{
   key:'Cbba-Uyuni',
-  value:126
+  value:515
 },{
   key:'Cbba-Uyuni (por Potosi)',
-  value:126
+  value:742
 },{
   key:'Cbba-Villamontes',
-  value:126
+  value:929
 },{
   key:'Cbba-Villazon',
-  value:126
+  value:928
 },{
   key:'Cbba-Yacuiba',
-  value:126
+  value:1026
 },{
   key:'Cbba-La Paz-Sucre',
-  value:126
+  value:1093
 },{
   key:'Cbba-La Paz-Sucre-Tarija',
-  value:126
+  value:1615
 },{
   key:'Cbba-La Paz-Tarija',
-  value:126
+  value:1291
 },{
   key:'Cbba-Oruro-La Paz',
-  value:126
+  value:447
 },{
   key:'Cbba-Oruro-Uyuni-Sucre',
-  value:126
+  value:862
 },{
   key:'Cbba-Pto. Suarez (carr. antigua)',
-  value:126
+  value:1141
 },{
   key:'Cbba-Pto. Suarez (corto)',
-  value:126
+  value:1111
 },{
   key:'Cbba-Pto. Suarez (largo)',
-  value:126
+  value:1514
 },{
   key:'Cbba-Sucre-Tarija',
-  value:126
+  value:1221
 },{
   key:'Cbba-Uyuni-Tarija',
-  value:126
+  value:1307
 },{
   key:'Cbba-Yacuiba (carr. antigua)',
-  value:126
+  value:1019
 },{
   key:'Oruro-Bermejo',
-  value:126
+  value:822
 },{
   key:'Oruro-La Paz',
-  value:126
+  value:239
 },{
   key:'Oruro-Tupiza',
-  value:126
+  value:639
 },{
   key:'Oruro-Potosi',
-  value:126
+  value:304
 },{
   key:'Oruro-Sucre',
-  value:126
+  value:451
 },{
   key:'Oruro-Tarija',
-  value:126
+  value:639
 },{
   key:'Oruro-Uyuni',
-  value:126
+  value:307
 },{
   key:'Oruro-Uyuni (por Potosi)',
-  value:126
+  value:504
 },{
   key:'Oruro-Villazon',
-  value:126
+  value:730
 },{
   key:'Potosi-La Paz',
-  value:126
+  value:543
 },{
   key:'Potosi-Tupiza',
-  value:126
+  value:335
 },{
   key:'Potosi-Uyuni',
-  value:126
+  value:200
 },{
   key:'Potosi-Villazon',
-  value:126
+  value:426
 },{
   key:'Pto. Villarroel-Santa Cruz',
-  value:126
+  value:270
 },{
   key:'Pto. Villarroel-Trinidad',
-  value:126
+  value:814
 },{
   key:'Santa Cruz-Bermejo',
-  value:126
+  value:1505
 },{
   key:'Santa Cruz-Bermejo (carr. antigua)',
-  value:126
+  value:1588
 },{
   key:'Santa Cruz-Camiri',
-  value:126
+  value:292
 },{
   key:'Santa Cruz-Cbba-La Paz-Tarija',
-  value:126
+  value:1759
 },{
   key:'Santa Cruz-La Paz',
-  value:126
+  value:867
 },{
   key:'Santa Cruz-La Paz (carr. antigua)',
-  value:126
+  value:881
 },{
   key:'Santa Cruz-Monteagudo',
-  value:126
+  value:358
 },{
   key:'Santa Cruz-Oruro',
-  value:126
+  value:683
 },{
   key:'Santa Cruz-Potosi',
-  value:126
+  value:1005
 },{
   key:'Santa Cruz-Potosi (carr. Antigua)',
-  value:126
+  value:1035
 },{
   key:'Santa Cruz-Pto. Suarez (corto)',
-  value:126
+  value:642
 },{
   key:'Santa Cruz-Pto. Suarez (largo)',
-  value:126
+  value:1046
 },{
   key:'Santa Cruz-San Jose (corto)',
-  value:126
+  value:263
 },{
   key:'Santa Cruz-San Jose (largo)',
-  value:126
+  value:671
 },{
   key:'Santa Cruz-Sucre (por Mizque - Aiquile)',
-  value:126
+  value:813
 },{
   key:'Santa Cruz-Sucre',
-  value:126
+  value:1134
 },{
   key:'Santa Cruz-Tarija',
-  value:126
+  value:1322
 },{
   key:'Santa Cruz-Trinidad',
-  value:126
+  value:544
 },{
   key:'Santa Cruz-Tupiza',
-  value:126
+  value:1322
 },{
   key:'Santa Cruz-Tupiza (carr. antigua)',
-  value:126
+  value:1299
 },{
   key:'Santa Cruz-Uyuni',
-  value:126
+  value:990
 },{
   key:'Santa Cruz-Uyuni (carr. antigua)',
-  value:126
+  value:1240
 },{
   key:'Santa Cruz-Uyuni (por Potosi)',
-  value:126
+  value:1210
 },{
   key:'Santa Cruz-Villamontes',
-  value:126
+  value:454
 },{
   key:'Santa Cruz-Villazon',
-  value:126
+  value:1413
 },{
   key:'Santa Cruz-Villazon (carr. antigua)',
-  value:126
+  value:1389
 },{
   key:'Santa Cruz-Yacuiba',
-  value:126
+  value:551
 },{
   key:'Sucre-Cbba',
-  value:126
+  value:659
 },{
   key:'Sucre-Potosi',
-  value:126
+  value:147
 },{
   key:'Sucre-Tupiza',
-  value:126
+  value:482
 },{
   key:'Sucre-Uyuni',
-  value:126
+  value:347
 },{
   key:'Sucre-Villazon',
-  value:126
+  value:573
 },{
   key:'Tarija-Bermejo',
-  value:126
+  value:183
 },{
   key:'Tarija-Tupiza',
-  value:126
+  value:191
 },{
   key:'Tarija-Villazon',
-  value:126
+  value:282
 },{
   key:'Villamontes-Yacuiba',
-  value:126
+  value:97
 },{
   key:'Santa Cruz-Cbba (crudo reducido)',
-  value:126
+  value:468
 },{
   key:'La Paz-Oruro-La Paz',
-  value:126
+  value:460
 },{
   key:'Santa Cruz-Pto. villarroel',
-  value:126
+  value:270
 },{
   key:'Santa Cruz-Sucre (por Potosi)',
-  value:126
+  value:1134
 },{
   key:'Cbba-Cbba',
-  value:126
+  value:10
 },{
   key:'La Paz-La Paz',
-  value:126
+  value:9
 },{
   key:'Santa Cruz-Santa Cruz',
-  value:126
+  value:16.5
 },{
   key:'Gyrm-Cobija (Brasil)',
-  value:126
+  value:620
 },{
   key:'La Paz-Cobija (Peru y Brasil)',
-  value:126
+  value:1024
 },{
   key:'Oruro-Cobija (Peru y Brasil)',
-  value:126
+  value:1395
 },{
   key:'Cbba-Cobija (Peru y Brasil)',
-  value:126
+  value:1556
 },{
   key:'Santa Cruz-Cobija (Peru y Brasil)',
-  value:126
+  value:2027
 },{
   key:'Gyrm-Cobija (Bolivia)',
-  value:126
+  value:486
 },{
   key:'Gyrm-Riberalta (automotriz)',
-  value:126
+  value:86
 },{
   key:'Gyrm-Riberalta (Aviacion)',
-  value:126
+  value:86
 },{
   key:'La Paz-Cobija (Bolivia)',
-  value:126
+  value:1254
 },{
   key:'La Paz-Riberalta',
-  value:126
+  value:951
 },{
   key:'Riberalta-Cobija',
-  value:126
+  value:400
 },{
   key:'Trinidad-Cobija',
-  value:126
+  value:1181
 },{
   key:'Trinidad-Guayaramerin',
-  value:126
+  value:970
 },{
   key:'Trinidad-Riberalta',
-  value:126
+  value:882
 },{
   key:'Gyrm-Gyrm',
-  value:126
+  value:1
 },{
   key:'Guabira-Santa Cruz',
-  value:126
+  value:75.6
 },{
   key:'Aguahi-Santa Cruz',
-  value:126
+  value:125.3
 }];
