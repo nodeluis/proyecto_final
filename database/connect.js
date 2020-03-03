c9c5cd69acbcc1cfbd26fa5e1579c9cc6173569b
luie1 <lruise.1@gmail.com>
2020-03-02 21:17:26 -0400

    contraseña arreglada

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 //mongoose.connect('mongodb://192.168.16.2:27017/veracruz');
 //mongodb+srv://veracruz:<password>@cluster0-rwbkh.mongodb.net/test
-mongoose.connect('mongodb+srv://veracruz:veracruz@cluster0-rwbkh.mongodb.net/test').then(()=>{
+mongoose.connect('mongodb+srv://veracruz:<veracruz>@cluster0-rwbkh.mongodb.net/test').then(()=>{
   console.log('conectado a cloud mongodb');
 }).catch(()=>{
   console.log('error al conectar mongodb');
