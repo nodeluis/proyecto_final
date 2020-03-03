f5a5a384d4d88e7d1d3b5d52f9bd3a1f861881d7
luie1 <lruise.1@gmail.com>
2020-03-02 20:54:44 -0400

    add mongo lab a mi app

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-
-mongoose.connect('mongodb://192.168.16.2:27017/veracruz');
+//mongoose.connect('mongodb://192.168.16.2:27017/veracruz');
+mongoose.connect('mongodb+srv://veracruz:veracruz@cluster0-rwbkh.mongodb.net/test');
 mongoose.set('useFindAndModify', false);
 
 module.exports=mongoose;
